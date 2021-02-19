f08d99d574f7ba44788d46b2d84d134ab3e7e7bb
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-19 15:38:59 -0700

    ingredient controller

--- Services/IngredientsService.cs
+++ Services/IngredientsService.cs
@@ -1,7 +1,28 @@
+using System;
+using allSpice.Models;
+
 namespace allSpice.Services
 {
-    public class IngredientsService
+  public class IngredientsService
+  {
+    internal object GetIngredientById(int id)
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal object CreateIngredient(Ingredient newIngredient)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object EditIngredient(Ingredient updatedIngredient)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void DeleteIngredient(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
