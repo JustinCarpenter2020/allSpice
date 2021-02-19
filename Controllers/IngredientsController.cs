d22a4ab83507c35d50bfc94f7056e3f593c9e594
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-19 15:39:08 -0700

    controller

--- Controllers/IngredientsController.cs
+++ Controllers/IngredientsController.cs
@@ -1,7 +1,82 @@
+using allSpice.Models;
+using allSpice.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace allSpice.Controllers
 {
-    public class IngredientsController
+        [ApiController]
+        [Route("api/[controller]")]
+    public class IngredientsController : ControllerBase
     {
-        
+        private readonly IngredientsService _service;
+        public IngredientsController(IngredientsService service)
+        {
+            _service = service;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Ingredient> getIngredientById(int id)
+        {
+            try
+            {
+                return Ok(_service.GetIngredientById(id));
+            }
+            catch (System.Exception err)
+            {
+                
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Ingredient> CreateIngredient([FromBody] Ingredient newIngredient)
+        {
+            try
+            {
+                return Ok(_service.CreateIngredient(newIngredient));
+            }
+            catch (System.Exception err)
+            {
+                
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Ingredient> EditIngredient([FromBody] Ingredient updatedIngredient, int id)
+        {
+            try
+            {
+                updatedIngredient.Id = id;
+                return Ok(_service.EditIngredient(updatedIngredient));
+            }
+            catch (System.Exception err)
+            {
+                
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<string> DeleteIngredient(int id)
+        {
+            try
+            {
+                _service.DeleteIngredient(id);
+                return Ok("Successfully removed")
+            }
+            catch (System.Exception err)
+            {
+                
+                return BadRequest(err.Message);
+            }
+        }
+
+
+
+
+
+
+
+
     }
 }
