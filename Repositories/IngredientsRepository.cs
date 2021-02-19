1383ad70eb384406dc3430955560171ef9c12f02
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-19 16:13:47 -0700

    ingredient service

--- Repositories/IngredientsRepository.cs
+++ Repositories/IngredientsRepository.cs
@@ -1,7 +1,34 @@
+using System;
+using System.Collections.Generic;
+using allSpice.Models;
+
 namespace allSpice.Repositories
 {
-    public class IngredientsRepository
+  public class IngredientsRepository
+  {
+    internal object GetById(int id)
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal object Create(Ingredient newIngredient)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object Edit(Ingredient updatedIngredient)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal IEnumerable<Recipe> GetByRecipeId(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Delete(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
