b3b9641b076f2a7c344eaee32935143b76a48002
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-19 16:32:52 -0700

    deleted

--- Repositories/RecipesRepository.cs
+++ Repositories/RecipesRepository.cs
@@ -1,7 +1,34 @@
+using System;
+using System.Collections.Generic;
+using allSpice.Models;
+
 namespace allSpice.Repositories
 {
-    public class RecipesRepository
+  public class RecipesRepository
+  {
+    internal IEnumerable<Recipe> GetAll()
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal Recipe GetById(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Recipe Create(Recipe newRecipe)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Recipe Edit(Recipe updatedRecipe)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Delete(Recipe data)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
