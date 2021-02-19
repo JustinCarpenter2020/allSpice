b2c4076775009665d0fb29ac02639fb6264ec677
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-02-19 15:24:52 -0700

    models

--- Models/Ingredient.cs
+++ Models/Ingredient.cs
@@ -2,6 +2,15 @@ namespace allSpice.Models
 {
     public class Ingredient
     {
-        
+    public string Quantity { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int Id { get; set; }
+
+        public int RecipeId { get; set; }
+
+        public Recipe Recipe { get; set; }
     }
 }
--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -2,6 +2,11 @@ namespace allSpice.Models
 {
     public class Recipe
     {
-        
+    public string Title { get; set; }
+        public string Description { get; set; }
+
+        public int PrepTime { get; set; }
+
+        public int Id { get; set; }
     }
 }
